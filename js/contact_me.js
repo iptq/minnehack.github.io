ebf5f894de0f030ff828354e43296a119ae6c056
Mitch Croal <mitch.cro@gmail.com>
2015-09-02 01:58:50 -0500

    More registration changes

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,15 +20,41 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            
-            var obj = { name: name,
-                        school: school,
-                        email: email,
-                        phone: phone,
-                        teamname: teamname,
-                        message: message }
 
-            console.log(obj)
+            var firebase_callback =
+              function(arg) {
+                if (arg == null) {
+                  // Show positive result
+                  $("#success").html("<div class='alert alert-success'>");
+                  $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                  $('#success > .alert-success')
+                    .append("<strong>Registration successful! </strong>");
+                  $('#success > .alert-success')
+                    .append('</div>');
+
+                  // Clear all fields
+                  $('#contactForm').trigger("reset");
+                } else {
+                  // Fail message
+                  $('#success').html("<div class='alert alert-danger'>");
+                  $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                  $('#success > .alert-danger').append("<strong>Registration failed. Try again in a minute, contact us if error persists.");
+                  $('#success > .alert-danger').append('</div>');
+                }
+              }
+
+            var registrationDB = new Firebase("https://minnehack-register.firebaseio.com");
+
+            var input = { name: name,
+                          school: school,
+                          email: email,
+                          phone: phone,
+                          teamname: teamname,
+                          message: message }
+
+            registrationDB.push(input, firebase_callback);
         },
         filter: function() {
             return $(this).is(":visible");
