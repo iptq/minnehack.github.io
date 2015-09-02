53611ec6086e65f3b01f8ffc7ddf05dc6c8b3318
Mitch Croal <mitch.cro@gmail.com>
2015-09-02 04:24:54 -0500

    More testing

---
+++ js/forms.js
@@ -0,0 +1,146 @@
+$(function() {
+    // Do bootstrap validation for the registration field
+    $("#registrationForm input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) {
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            
+            var name =     $("#registrationForm input#name").val();
+            var school =   $("#registrationForm input#school").val();
+            var email =    $("#registrationForm input#email").val();
+            var phone =    $("#registrationForm input#phone").val();
+            var teamname = $("#registrationForm input#teamname").val();
+            var message =  $("#registrationForm textarea#message").val();
+
+            var input = { name: name,
+                          school: school,
+                          email: email,
+                          phone: phone,
+                          teamname: teamname,
+                          message: message }
+
+            for (field in input) {
+              // Filter out empty, non-required fields
+              if (input.hasOwnProperty(field) &&
+                  !input[field]) {
+                delete input[field];
+              }
+            }
+
+            console.log("Registering...");
+            console.log(input);
+            if (true) {
+              return;
+            }
+
+            var firebase_callback =
+              function(arg) {
+                if (arg == null) {
+                  // Show positive result
+                  $("#registration-status").html("<div class='alert alert-success'>");
+                  $('#registration-status > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                  $('#registration-status > .alert-success')
+                    .append("<strong>Registration successful! </strong>");
+                  $('#registration-status > .alert-success')
+                    .append('</div>');
+
+                  // Clear all fields
+                  $('#contactForm').trigger("reset");
+                } else {
+                  // Fail message
+                  $('#registration-status').html("<div class='alert alert-danger'>");
+                  $('#registration-status > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    .append("</button>");
+                  $('#registration-status > .alert-danger').append("<strong>Registration failed. Try again in a minute, contact us if error persists.");
+                  $('#registration-status > .alert-danger').append('</div>');
+                }
+              }
+
+            var registrationDB = new Firebase("https://minnehack-register.firebaseio.com");
+
+
+            registrationDB.push(input, firebase_callback);
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("#contactForm input,textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+            var name =     $("#contactForm input#name").val();
+            var email =    $("#contactForm input#email").val();
+            var message =  $("#contactForm textarea#message").val();
+
+            var firstName = name; // For Success/Failure Message
+            // Check for white space in name for Success/Fail message
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
+
+            console.log("Sending email..." + message);
+            return;
+
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    name: name,
+                    phone: phone,
+                    email: email,
+                    message: message
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#contactForm').trigger("reset");
+                },
+            })
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+
+/*When clicking on Full hide fail/success boxes */
+$('#registrationForm input#name').focus(function() {
+    $('#registration-status').html('');
+});
+
+$('#contactForm input#name').focus(function() {
+    $('#contact-status').html('');
+});
